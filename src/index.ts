3459ec822bd6c746268a9c409e688f83943060c3
jdeepee <joshuadparkin@gmail.com>
2021-06-18 14:04:12 +0100

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from "./address/Address";
 export * from "./agent/Agent";
-export * from "./agent-service/AgentService";
 export * from "./expression/Expression";
 export * from "./expression/ExpressionRef";
 export * from "./language/Language";
@@ -8,5 +7,7 @@ export * from "./language/LanguageRef";
 export * from "./language/LanguageContext";
 export * from "./links/Links";
 export * from "./perspectives/Perspective";
-export * from "./perspectives/SharedPerspective";
 export * from "./signature-service/SignaturesService";
+export * from "./agent-service/AgentService";
+export * from "./neighbourhood/Neighbourhood";
+export * from "./social-organism/SocialOrganism";
