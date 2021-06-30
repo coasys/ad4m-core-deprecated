28b0bafb2f352b3a4d3a12a4669fac671066b06d
Nicolas Luck <nicolas@lucksus.eu>
2021-06-30 16:48:31 +0200

    Add directMessageLanguage to Agent

--- src/agent/Agent.ts
+++ src/agent/Agent.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType } from "type-graphql";
+import LanguageRef from "../language/LanguageRef";
 import Perspective from "../perspectives/Perspective";
 
 // An Agent (as seen from other Agents)
@@ -8,14 +9,21 @@ import Perspective from "../perspectives/Perspective";
 // in the DID document)
 // 
 // 2. Holds and shares a Perspective that links all information
-// this agent wants to offer as public-facing semantics
+// this agent wants to offer as public-facing semantics.
+// This should be used for any kind of user profile information.
+//
+// 3. A reference to the direct message language via which the agent
+// is able to receive DMs.
 @ObjectType()
 export default class Agent {
     @Field()
     did: String;
 
-    @Field(type => Perspective)
+    @Field(type => Perspective, {nullable: true})
     perspective: Perspective;
+
+    @Field({nullable: true})
+    directMessageLanguage: LanguageRef
 
     constructor(did: string, perspective: Perspective) {
         this.did = did
