4364833a74e3696552f8893f73ebf1815883be96
Nicolas Luck <nicolas@lucksus.eu>
2021-10-06 21:33:06 +0200

    Add local dep. link to ad4m in check readme project

--- scripts/addLocalAd4mDep.js
+++ scripts/addLocalAd4mDep.js
@@ -1,1 +1,7 @@
-console.log("addLocalAd4mDep test")
+const fs = require('fs')
+
+const pkg = JSON.parse(fs.readFileSync('package.json').toString())
+if(!pkg['dependencies'])
+  pkg['dependencies'] = {}
+pkg['dependencies']['@perspect3vism/ad4m'] = "file:../../"
+fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
--- scripts/runReadmeJs.sh
+++ scripts/runReadmeJs.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 pwd
 cd tmp/readme-test
 npm init -y
+node addLocalAd4mDep.js
 bash readmeCodeSetup.sh
 
 npx ts-node readmeCode.ts
